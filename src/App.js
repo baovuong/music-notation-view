132e178c026664b51f88ad91971365b6b39a467d
Bao Vuong <vqbao@lv5.org>
2019-06-16 21:35:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import StandardNotationDisplay from './StandardNotationDisplay';
 
 function App() {
+  
+  let scales = [
+    {
+      intervals: [2, 2, 1, 3, 1, 2, 1],
+      root: 0,
+      tones: 12,
+      names: ['Ionian sharp 5', 'Ionian Augmented']
+    },
+    {
+      "intervals": [
+          2, 
+          1, 
+          2, 
+          2, 
+          2, 
+          1, 
+          2
+      ], 
+      "root": 0, 
+      "names": [
+          "G.Phrygian", 
+          "M.Dorian", 
+          "M.Hypomixolydian", 
+          "Kafi That", 
+          "Mela Kharaharapriya", 
+          "Raga Bageshri", 
+          "Bhimpalasi", 
+          "Nayaki Kanada", 
+          "Sriraga", 
+          "Ritigaula", 
+          "Huseni", 
+          "Kanara", 
+          "Mischung 5", 
+          "Gregorian nr.8", 
+          "Eskimo Heptatonic", 
+          "Yu: China", 
+          "Hyojo", 
+          "Oshikicho", 
+          "Banshikicho: Japan", 
+          "Nam: Vietnam"
+      ], 
+      "tones": 12
+  }
+  ];
+
+  let useFlats = false;
+  let startingNote = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="musicScales">
+        {scales.map((scale) =>
+          <div>
+            <StandardNotationDisplay
+              scale={scale}
+              startingNote={startingNote}
+              useFlats={useFlats}
+            />
+            {scale.names.map((name) => <div>{name}</div>)}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
